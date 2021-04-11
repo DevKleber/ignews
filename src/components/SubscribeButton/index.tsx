28a328e4b89f4ffc7b6e9fb163e2fd237bc32d0a
kleber <kleber107@gmail.com>
2021-04-11 17:36:15 -0300

    configurando Stripe, Alterando cliente no faunaDB, cadastrando cliente no Stripe e redirecionando para o checkout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -8,20 +8,25 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+	//obtendo usuario logado.
 	const [session] = useSession();
 
+	//função clique no botão subscribe now.
 	async function handleSubscribe() {
+		//caso usuario não esteja logado, faço o redirect para o login.
 		if (!session) {
 			signIn("github");
 			return;
 		}
 
 		//criação do checkout session
-
 		try {
+			// faço uma requisição post para o serviço criado (src/pages/api/subscribe.ts)
 			const response = await api.post("/subscribe");
+			//obtendo o sessionId
 			const { sessionId } = response.data;
 
+			//fazendo redirection para o Stripe checkout.
 			const stripe = await getStripeJs();
 			stripe.redirectToCheckout({ sessionId });
 		} catch (err) {
--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -17,8 +17,13 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 	if (req.method === "POST") {
 		//criando usuario no Stripe
 
+		// acessando nosso next-auth vamos recuperar a sessao do usuario logado.
 		const session = await getSession({ req });
 
+		/**
+		 * Para não duplicar usuarios dentro do nosso Stripe, vamos consultar nosso banco
+		 * obter o stripe_customer_id e verificar se existe valor dentro.
+		 */
 		//recuperando usuario dentro do banco fauna.
 		const user = await fauna.query<User>(
 			q.Get(
@@ -31,13 +36,22 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 			)
 		);
 
+		//passando o valor obtido do nosso banco de dados referente ao ID do Stripe
 		let customerId = user.data.stripe_customer_id;
 
+		//verificando se esse cliente já tem conta no Stripe
+		//Caso não tenha
 		if (!customerId) {
+			//Criando uma conta para ele no banco do Stripe
 			const stripeCustomer = await stripe.customers.create({
 				email: session.user.email,
 				// meta
 			});
+
+			/**
+			 * Conta criada, devemos informar nosso banco de dados FaunaDB que esse cliente
+			 * agora tem uma conta no Stripe
+			 */
 
 			//adicionando ID do Stripe para dentro do nosso banco faunaDB
 			await fauna.query(
@@ -48,23 +62,28 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 				})
 			);
 
+			/**
+			 * Com tudo ocorrendo certo, devo mudar o valor do customerId, pq agora
+			 * esse cliente tem conta no Stripe.
+			 */
 			customerId = stripeCustomer.id;
 		}
 
+		//Mandando cliente para tela de checkout do Stripe
 		const stripeCheckoutSession = await stripe.checkout.sessions.create({
-			customer: customerId,
-			payment_method_types: ["card"],
-			billing_address_collection: "required",
+			customer: customerId, // Cliente que vai realizar o pagamento
+			payment_method_types: ["card"], // metodos de pagamento que irei aceitar.
+			billing_address_collection: "required", // Endereço é obrigatorio.
 			line_items: [
 				{
 					price: "price_1If3DVH1bObiaepXJbs5MVZ3",
 					quantity: 1,
 				},
 			],
-			mode: "subscription",
-			allow_promotion_codes: true,
-			success_url: "http://localhost:3000/pots",
-			cancel_url: "http://localhost:3000",
+			mode: "subscription", // Cobrança recorrente.
+			allow_promotion_codes: true, // permite códigos promocionais.
+			success_url: "http://localhost:3000/pots", //caso ele faça o pagamento mando para
+			cancel_url: "http://localhost:3000", //caso de erro mando para.
 		});
 
 		return res.status(200).json({ sessionId: stripeCheckoutSession.id });
