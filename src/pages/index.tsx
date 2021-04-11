3c2d35f85bda2eb7c44e46c621dd0fde14577fd3
kleber <kleber107@gmail.com>
2021-04-11 11:42:17 -0300

    Static Site Generation (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
@@ -36,7 +36,7 @@ export default function Home({ product }: HomeProps) {
 	);
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 	const price = await stripe.prices.retrieve(
 		"price_1If3DVH1bObiaepXJbs5MVZ3",
 		{
@@ -55,5 +55,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
 		props: {
 			product,
 		},
+		//Quanto tempo em segundos devo refazer a busca na API
+		revalidate: 60 * 60 * 24, // 24 Horas
 	};
 };
